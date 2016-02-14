27dd22cfafe8556c88ae4b5a993bbc0830788f15
Luis Angel Sustaita <luis.sustaita@cimat.mx>
2016-02-14 10:01:34 -0600

    googleVis BarCharts

--- RGraphs/BarCharts/GoogleVisBarChart.R
+++ RGraphs/BarCharts/GoogleVisBarChart.R
@@ -1,7 +1,7 @@
 library(googleVis)
 df=data.frame(country=c("Computer",  "Tv", "Telephone","Smartwatch"), 
               val1=c(18,12,25,5))
-Combo <- gvisComboChart(df, xvar="country",
+Combo <- gvisComboChart(df, xvar="Most Used Technology",
                         yvar=c("val1"),
                         options=list(seriesType="bars",
                                      series='{1: {type:"line"}}'))
